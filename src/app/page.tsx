831a27360d43ab17be5832081d15674cb5cb72ef
Abhishek Biswal <wirddin@gmail.com>
2024-01-10 16:47:38 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
       <HomeOurMissionHighlighted />
       <HomeBlogSection />
-      <HomeOurClients />
+      {/* <HomeOurClients /> */}
       <PFooter />
     </main>
   );
