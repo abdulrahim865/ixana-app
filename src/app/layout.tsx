157ded6a05cb745517044bcaed5620450468779c
Abhishek Biswal <wirddin@gmail.com>
2024-01-11 19:20:23 +0530

    Updated head tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
+import Script from "next/script";
 
 const AspektaVF = localFont({
   src: [
@@ -28,18 +29,32 @@ const AspektaVF = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Ixana",
-  description:
-    "Augmenting humans with all-day, real-time, distributed AI on what you see,hear and sense",
+  title: "Ixana: High-speed Human Computer Interaction",
+  description: "Augmenting humans with all-day, real-time, distributed AI on what you see,hear and sense",
+  keywords:
+    "Augmented Reality, Virtual Reality, Metaverse, AI, Artificial Intelligence, Low Power Wireless, IoT, Semiconductor, Communication, Wireless",
+  robots: "index, follow",
+  openGraph: {
+    url: "https://ixana.ai",
+    type: "website",
+    title: "Ixana: High-speed Human Computer Interaction",
+    description: "Augmenting humans with all-day, real-time, distributed AI on what you see,hear and sense",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ixana: High-speed Human Computer Interaction",
+    description: "Augmenting humans with all-day, real-time, distributed AI on what you see,hear and sense",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* External Scripts */}
+      <Script type="text/javascript" src="https://www.clarity.ms/s/0.7.20/clarity.js" async />
+      <Script type="text/javascript" src="https://app.posthog.com/static/array.js" async />
+      <Script type="text/javascript" src="https://www.clarity.ms/tag/ermnjbk1lg" async />
+      <Script type="text/javascript" src="https://www.googletagmanager.com/gtag/js?id=G-KX71MVT6XV" async />
       <body className={AspektaVF.className}>{children}</body>
     </html>
   );
