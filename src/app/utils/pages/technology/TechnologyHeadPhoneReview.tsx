9ba0fd02cac813541389cbb3cd3403f977e6814f
Abhishek Biswal <wirddin@gmail.com>
2024-02-28 18:52:50 +0530

    misc fixes

--- src/app/utils/pages/technology/TechnologyHeadPhoneReview.tsx
+++ src/app/utils/pages/technology/TechnologyHeadPhoneReview.tsx
@@ -1,10 +1,29 @@
 "use client";
 
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Mousewheel, Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
+
+const slides = [
+  {
+    title: "Headphones",
+    text: "Test",
+    image: "/assets/technology/headset-preview.png",
+  },
+  {
+    title: "Headphones",
+    text: "Test",
+    image: "/assets/technology/headset-preview.png",
+  },
+  {
+    title: "Headphones",
+    text: "Test",
+    image: "/assets/technology/headset-preview.png",
+  },
+];
 
 export default function TechnologyHeadPhoneReview() {
   return (
@@ -13,19 +32,55 @@ export default function TechnologyHeadPhoneReview() {
       slidesPerView={1}
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
-      className="w-full h-[50vh] bg-gray-200"
+      className="w-full h-[80vh] bg-gray-200"
       direction="vertical"
       mousewheel={{ forceToAxis: true, sensitivity: 1, releaseOnEdges: true }}
       pagination={{
+        enabled: true,
         clickable: true,
+        bulletActiveClass: "swiper-pagination-bullet-active !bg-white",
+        bulletClass: "swiper-pagination-bullet !opacity-100 !bg-gray-500",
       }}
       modules={[Mousewheel, Pagination]}
+      effect="fade"
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {slides.map((props, index) => (
+        <SwiperSlide key={index}>
+          <Slide {...props} />
+        </SwiperSlide>
+      ))}
     </Swiper>
+  );
+}
+
+function Slide({ image, text, title }: { image: string; text: string; title: string }) {
+  return (
+    <div className=" overflow-hidden relative flex flex-col w-full h-full grow bg-iblack ">
+      <div className="absolute top-0 bottom-0 -left-[5%] -right-[5%] flex items-end  gap-5 justify-end">
+        <Image
+          src={image}
+          alt="Head Phones Ixana"
+          className="flex w-auto h-full max-h-full lg:max-w-[100%]"
+          width={500}
+          height={500}
+          priority
+        />
+      </div>
+      <div className="absolute flex flex-col items-start gap-5 text-white left-10 bottom-10">
+        <Image
+          src="/assets/technology/tech-wireless.svg"
+          alt="svg tech wireless"
+          className="flex w-5 h-5"
+          width={22}
+          height={22}
+        />{" "}
+        <h1 className="flex pt-5 text-5xl font-light ">{title}</h1>
+        <span className=" text-xs font-light max-w-[400px]">{text} </span>
+        {/* <a href="" className="pt-5 text-xs font-light ">
+      Skip This Section
+    </a> */}
+      </div>
+    </div>
   );
 }
 
