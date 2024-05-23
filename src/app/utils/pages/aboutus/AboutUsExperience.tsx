9a73c5880ca38a6556f9d9149fd54a8e72160e81
Abhishek Biswal <wirddin@gmail.com>
2024-05-23 17:55:02 +0530

    About Us Changes

--- src/app/utils/pages/aboutus/AboutUsExperience.tsx
+++ src/app/utils/pages/aboutus/AboutUsExperience.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import { Pbutton, ToprightArrow } from "../../components/Pbutton";
+import Image from "next/image";
 
 export default function AboutUsExperience() {
   return (
     <div className="flex min-h-[60vh] w-full flex-col  container items-center justify-center py-12">
       <div className="flex flex-col w-full gap-12 lg:flex-row">
         <div className="flex lg:items-center lg:justify-center basis-1/2">
-          <h2 className="flex text-4xl font-light lg:max-w-[350px] ">7 years of Research and Development</h2>
+          <Image src={"/assets/aboutus/7years.png"} width={600} height={300} alt="Image" />
         </div>
-        <div className="flex flex-col items-start gap-5 basis-1/2">
+        <div className="flex flex-col items-start gap-5 basis-1/2 pt-5">
+          <h2 className="flex text-4xl font-light lg:max-w-[350px] ">7 years of Research and Development</h2>
           <p className="text-xs font-light ">
             We are often glued to our smartphones. Yet, we mostly interact with the smartphone &quot;computer&quot; by
             looking at a tiny 6-inch screen. The speed of today&apos;s human-computer interaction (HCI) is slow, similar
